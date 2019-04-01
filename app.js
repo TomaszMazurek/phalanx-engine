797bedaf36ed57ba404a5e052afd2ee8717a3b3f
Tomasz Mazurek <Tomasz.A.Mazurek@gmail.com>
2019-04-01 19:28:21 +0200

    Shadows for DirectionalLight. For point light it was necessary to simulate it with spotlight

--- app.js
+++ app.js
@@ -2,18 +2,25 @@ var scene, camera, renderer, stats, controls, gui, textureEvent,
     params = {
         speed : 0.001,
         pointLight1Power: 1.2,
+        pointLight1Shadow: true,
         pointLight2Power: 1.2,
+        pointLight2Shadow: true,
         directionalLight: 1.2,
+        directionalLightShadow: true,
         bumpScale : 1.0,
         roughness : 0.5,
         shininess : 0.5,
-        texture: "wood1"
+        texture: "wood1",
+        near : 500,
+        far : 25000,
+        fov : 30
 };
 var geometry, material, plane, texture,
     normalMap, specularMap, roughnessMap,
     mesh, phongMaterial, stdMaterial, normalMaterial,
     meshPhong, meshStandard, meshNormal,
-    pointLight1, pointLight2, directionalLight;
+    pointLight1, pointLight2, directionalLight,
+    near,far, fov;
 
 
 var textureLoader = new THREE.TextureLoader();
@@ -35,9 +42,10 @@ var textureMap = {
 };
 
 function init() {
-    createUI()
     createScene();
+    createUI();
     createLight();
+    createShadow();
     createObjects();
     document.getElementById("scene-container").appendChild( renderer.domElement );
     document.body.appendChild( stats.domElement );
@@ -63,12 +71,27 @@ function animate() {
 
     meshNormal.rotation.x += params.speed;
     meshNormal.rotation.y += params.speed;
-    meshNormal.material.bumpScale = params.bumpScale;
-    meshNormal.material.roughness = params.roughness;
+
+    directionalLight.intensity = params.directionalLight;
+    directionalLight.castShadow = params.directionalLightShadow;
+
+    directionalLight.shadowCameraNear = params.near;
+    directionalLight.shadowCameraFar = params.far;
+    directionalLight.shadowCameraFov = params.fov;
 
     pointLight1.intensity = params.pointLight1Power;
+    pointLight1.castShadow = params.pointLight1Shadow;
+
+    pointLight1.shadowCameraNear = params.near;
+    pointLight1.shadowCameraFar = params.far;
+    pointLight1.shadowCameraFov = params.fov;
+
     pointLight2.intensity = params.pointLight2Power;
-    directionalLight.intensity = params.directionalLight;
+    pointLight2.castShadow = params.pointLight2Shadow;
+
+    pointLight2.shadowCameraNear = params.near;
+    pointLight2.shadowCameraFar = params.far;
+    pointLight2.shadowCameraFov = params.fov;
 
     stats.end();
 
@@ -80,8 +103,8 @@ function animate() {
 function createScene() {
     renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
-    renderer.shadowMap.enabled = true;
-    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    renderer.shadowMapEnabled = true;
+    renderer.shadowMapSoft = true;
 
     scene = new THREE.Scene();
     scene.background = new THREE.Color( 'skyblue' );
@@ -96,48 +119,103 @@ function createScene() {
     controls = new THREE.OrbitControls( camera, document.getElementById("scene-container"));
 }
 function createLight() {
-
     //--------------------------light-------------------------------
-    var ambientLight = new THREE.AmbientLight( 0x404040 );
-    scene.add( ambientLight );
+    //scene.add( new THREE.AmbientLight( 0x404040 ) );
 
     // White directional light at half intensity shining from the top.
-    directionalLight = new THREE.DirectionalLight( 0xffffff, 1.0 );
-    directionalLight.position = new THREE.Vector3(1,1,1);
-    directionalLight.shadowMapWidth = directionalLight.shadowMapHeight = 2048;
-    directionalLight.shadowDarkness = 0.5;
-    directionalLight.castShadow = true;
-    directionalLight.shadowCameraVisible = true;
+    directionalLight = new THREE.DirectionalLight(0xdfebff, 1.75);
+    directionalLight.position.set(300, 600, 50);
+    directionalLight.position.multiplyScalar(1.3);
 
-    directionalLight.shadow.mapSize.width = 512;  // default
-    directionalLight.shadow.mapSize.height = 512; // default
-    directionalLight.shadow.camera.near = 0.5;    // default
-    directionalLight.shadow.camera.far = 500;     // default
-    scene.add( directionalLight );
+    scene.add(directionalLight);
 
-    pointLight1 = new THREE.PointLight(0xffffff, 1.0);
-    pointLight1.position.set( -200, -100, 200 );
-    pointLight1.castShadow = true;
+    var dirLightHelper = new THREE.DirectionalLightHelper( directionalLight, 100 );
+    scene.add( dirLightHelper );
+
+    pointLight1 = new THREE.SpotLight( 0xffffff );
+    pointLight1.position.set( 200,300, 400);
+    pointLight1.angle = 180;
     scene.add( pointLight1 );
 
     var sphereSize = 10;
     var pointLightHelper1 = new THREE.PointLightHelper( pointLight1, sphereSize );
     scene.add( pointLightHelper1 );
 
-    pointLight2 = new THREE.PointLight( 0xffffff, 1.0 );
-    pointLight2.position.set(200,-100, 200);
-    pointLight2.castShadow = true;
+    pointLight2 = new THREE.SpotLight( 0xffffff );
+    pointLight2.position.set( -200,300, -400);
+    pointLight2.angle = 180;
+
     scene.add( pointLight2 );
 
     var sphereSize = 10;
-    var pointLightHelper1 = new THREE.PointLightHelper( pointLight2, sphereSize );
-    scene.add( pointLightHelper1 );
+    var pointLightHelper2 = new THREE.PointLightHelper( pointLight2, sphereSize );
+    scene.add( pointLightHelper2 );
+}
+function createShadow(){
+
+    var d = 2000;
+
+//directionaLight
+    directionalLight.castShadow = true;
+    directionalLight.shadowCameraVisible = true;
+
+    directionalLight.shadowMapWidth = 1024;
+    directionalLight.shadowMapHeight = 1024;
+
+    directionalLight.shadowCameraNear = params.near;
+    directionalLight.shadowCameraFar = params.far;
+    directionalLight.shadowCameraFov = params.fov;
+
+    directionalLight.shadowCameraLeft = -d;
+    directionalLight.shadowCameraRight = d;
+    directionalLight.shadowCameraTop = d;
+    directionalLight.shadowCameraBottom = -d;
+
+    directionalLight.shadowDarkness = 0.2;
+
+//point light 1
+    pointLight1.castShadow = true;
+    pointLight1.shadowCameraVisible = true;
+
+    pointLight1.shadowMapWidth = 1024;
+    pointLight1.shadowMapHeight = 1024;
+
+    pointLight1.shadowCameraNear = params.near;
+    pointLight1.shadowCameraFar = params.far;
+    pointLight1.shadowCameraFov = params.fov;
+
+    pointLight1.shadowCameraLeft = -d;
+    pointLight1.shadowCameraRight = d;
+    pointLight1.shadowCameraTop = d;
+    pointLight1.shadowCameraBottom = -d;
+
+    pointLight1.shadowDarkness = 0.2;
+    pointLight1.shadowBias = 0.001;
+
+//point light 2
+    pointLight2.castShadow = true;
+    pointLight2.shadowCameraVisible = true;
+
+    pointLight2.shadowMapWidth = 1024;
+    pointLight2.shadowMapHeight = 1024;
+
+    pointLight2.shadowCameraNear = params.near;
+    pointLight2.shadowCameraFar = params.far;
+    pointLight2.shadowCameraFov = params.fov;
+
+    pointLight2.shadowCameraLeft = -d;
+    pointLight2.shadowCameraRight = d;
+    pointLight2.shadowCameraTop = d;
+    pointLight2.shadowCameraBottom = -d;
+
+    pointLight2.shadowDarkness = 0.2;
+    pointLight2.shadowBias = 0.001;
+
 }
 function createObjects() {
     texture = new THREE.TextureLoader().load( "textures/wood/wood1/Base_Color.jpg" );
     normalMap = new THREE.TextureLoader().load( "textures/wood/wood1/Normal.jpg" );
     roughnessMap = new THREE.TextureLoader().load( "textures/wood/wood1/Roughness.jpg" );
-
 
     //phong object
     geometry = new THREE.BoxGeometry( 150, 150, 150 );
@@ -172,7 +250,6 @@ function createObjects() {
     meshStandard.receiveShadow = false;
     scene.add( meshStandard );
 
-
     //normal object
     geometry = new THREE.BoxGeometry(150, 150, 150 );
     normalMaterial = new THREE.MeshLambertMaterial({
@@ -194,26 +271,25 @@ function createObjects() {
     //var planeGeometry = new THREE.PlaneGeometry( 2000, 2000);
     var planeGeometry = new THREE.BoxGeometry(2000, 2000, 10, 100, 100, 5);
     var planeMaterial = new THREE.MeshPhongMaterial( {
-        color: 0xffffff,
+        color: 0xCD8500,
         side: THREE.DoubleSide,
         map: new THREE.TextureLoader().load( "textures/wood/wood3/Base_Color.jpg",    async function ( map ) {
             textureMap["checker"][2] = map;
             map.name = "checker";
             map.wrapS = map.wrapT = THREE.RepeatWrapping;
             map.anisotropy = 16;
-            map.offset.set( 0, 0 );
-            map.repeat.set( 1, 1 );
+            map.repeat.set(4, 4);
         })} );
     plane = new THREE.Mesh( planeGeometry, planeMaterial );
     plane.rotation.x = Math.PI / 2;
     plane.position.y = -150;
+    plane.material.normalMap = new THREE.TextureLoader().load( "textures/wood/wood3/Normal.jpg" );
+    plane.material.normalMap.wrapS = plane.material.normalMap.wrapT = THREE.RepeatWrapping;
+    plane.material.normalMap.repeat.set(4, 4);
+    plane.material.bumpMap = new THREE.TextureLoader().load( "textures/wood/wood3/Bump.jpg" );
+    plane.material.bumpMap.wrapS = plane.material.bumpMap.wrapT = THREE.RepeatWrapping;
+    plane.material.bumpMap.repeat.set(4, 4);
     plane.receiveShadow = true;
-    /*
-        plane.material.map.wrapS = THREE.RepeatWrapping;
-        plane.material.map.wrapT = THREE.RepeatWrapping;
-        plane.material.map.anisotropy = 16;
-        plane.material.map.repeat.set( 1, 1 );*/
-
     scene.add( plane );
 }
 function createUI(){
@@ -222,13 +298,33 @@ function createUI(){
     stats.showPanel( 0 ); // 0: fps, 1: ms, 2: mb, 3+: custom
 //dat
     gui = new dat.GUI();
-    gui.add(params, 'speed', -0.1, 0.1).name('speed');
-    gui.add(params, 'pointLight1Power', 0.0, 3.0);
-    gui.add(params, 'pointLight2Power', 0.0, 3.0);
-    gui.add(params, 'directionalLight', 0.0, 3.0);
-    gui.add(params, 'bumpScale', -1.0, 1.0);
-    gui.add(params, 'roughness', 0.0, 1.0);
-    gui.add(params, 'shininess', 0.0, 1.0);
+
+    var objects = gui.addFolder('Objects');
+    objects.add(params, 'speed', -0.1, 0.1).name('speed');
+
+    function updateCamera() {
+        camera.updateProjectionMatrix();
+    }
+    var view = gui.addFolder('Camera');
+
+    view.add(camera, 'fov', 1, 180).onChange(updateCamera);
+    const minMaxGUIHelper = new MinMaxGUIHelper(camera, 'near', 'far', 0.1);
+    view.add(minMaxGUIHelper, 'min', 0.1, 10000, 0.1).name('near').onChange(updateCamera);
+    view.add(minMaxGUIHelper, 'max', 0.1, 25000, 0.1).name('far').onChange(updateCamera);
+
+    var lights = gui.addFolder('Lights');
+    lights.add(params, 'pointLight1Power', 0.0, 3.0);
+    lights.add(params, 'pointLight1Shadow');
+    lights.add(params, 'pointLight2Power', 0.0, 3.0);
+    lights.add(params, 'pointLight2Shadow');
+    lights.add(params, 'directionalLight', 0.0, 3.0);
+    lights.add(params, 'directionalLightShadow');
+
+    var textures = gui.addFolder('Textures');
+    textures.add(params, 'bumpScale', -1.0, 1.0);
+    textures.add(params, 'roughness', 0.0, 1.0);
+    textures.add(params, 'shininess', 0.0, 1.0);
+
     textureEvent = gui.add(params, 'texture', [ 'wood1', 'wood2','wood3', 'cobble1','cobble2',
         'cobble3','roof1', 'roof2','roof3', 'bricks1','bricks2', 'bricks3', 'iceTexture'] );
     textureEvent.onChange(function(value) {
@@ -346,11 +442,8 @@ function applyMaterial(value){
     meshStandard.material.needsUpdate = true;
 
     //Lambert
-    meshNormal.material = new THREE.MeshStandardMaterial( {
+    meshNormal.material = new THREE.MeshLambertMaterial( {
         map: textureMap[value][2],
-        bumpMap : textureMap[value][3],
-        normalMap : textureMap[value][4],
-        roughnessMap : textureMap[value][5],
         aoMap : textureMap[value][6]
     });
     meshNormal.material.needsUpdate = true;
@@ -362,6 +455,29 @@ function applyMaterial(value){
     meshStandard.material.name = value;
     meshNormal.material.name = value;
 }
+class MinMaxGUIHelper {
+    constructor(obj, minProp, maxProp, minDif) {
+        this.obj = obj;
+        this.minProp = minProp;
+        this.maxProp = maxProp;
+        this.minDif = minDif;
+    }
+    get min() {
+        return this.obj[this.minProp];
+    }
+    set min(v) {
+        this.obj[this.minProp] = v;
+        this.obj[this.maxProp] = Math.max(this.obj[this.maxProp], v + this.minDif);
+    }
+    get max() {
+        return this.obj[this.maxProp];
+    }
+    set max(v) {
+        this.obj[this.maxProp] = v;
+        this.min = this.min;  // this will call the min setter
+    }
+}
+
 var textureMapPromise = new Promise(async function(resolve, reject) {
     await populateTextureMap();
     resolve();
